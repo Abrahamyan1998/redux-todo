01a5b2a40c3e6b1f604e012d387631f0323b40b8
Taron1998 <taron.abrahamyan.2018>
2023-03-22 23:35:45 -0700

    redux todo

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { addToDo, removeToDo } from './Components/Actions/Action';
+
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  const [todo, setTodo] = useState('') 
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
+
+  const todoSubmit = () => {
+    if (todo !== '') {
+      dispatch(addToDo(counter, todo))
+      setCounter(counter + 1)
+      setTodo('')
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <div className='todo'>
+        <input type='text' value={todo} onChange={(e) => setTodo(e.target.value)} />
+      </div>
+      <button onClick={todoSubmit}>add</button>
+      <div className='todos'>
+        {todos?.map((todo) => (
+          <li key={todo.id}>
+            <p>{todo.task}</p>
+            <button onClick={() => dispatch(removeToDo(todo.id))}>remove</button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+      
